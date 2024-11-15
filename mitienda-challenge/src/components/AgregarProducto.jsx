aaa077b56cc00561f81ca7347aec0b150060dbb6
Mateo Gomez <122096768+mateogomezrgg@users.noreply.github.com>
2024-11-15 10:31:59 -0300

    agregué el componente para agregarProducto

--- mitienda-challenge/src/components/AgregarProducto.jsx
+++ mitienda-challenge/src/components/AgregarProducto.jsx
@@ -1,15 +1,40 @@
+import { useState } from 'react';
+
 export const AgregarProducto = () => {
+  const [cantProducto, setCantProducto] = useState('');
+  const [idProducto, setIdProducto] = useState('');
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('cantidad de producto: ' + cantProducto);
+    console.log('id del producto: ' + idProducto);
+  };
+
   return (
     <div className='agregarProducto'>
       <h3 className='titulo__agregarProducto'>
         Agrega los productos al carro del compra
       </h3>
 
-      <form>
-        <input type='number' placeholder='Cantidad' id='cantProducto' />
-        <input type='number' placeholder='ID del Producto' id='idProducto' />
+      <form onSubmit={onFormSubmit}>
+        <input
+          type='number'
+          placeholder='Cantidad'
+          id='cantProducto'
+          value={cantProducto}
+          onChange={(e) => setCantProducto(e.target.value)}
+        />
+        <input
+          type='number'
+          placeholder='ID del Producto'
+          id='idProducto'
+          value={idProducto}
+          onChange={(e) => setIdProducto(e.target.value)}
+        />
 
-        <button type='submit'>Agregar</button>
+        <button type='submit' onSubmit={onFormSubmit}>
+          Agregar
+        </button>
       </form>
     </div>
   );
